8e1908d58c59fd760627f19eddbfc6304f4bb398
aliakseividaseu <alexaat@yahoo.co.uk>
2025-03-01 21:11:37 +0000

    google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:friends_tracker_app/pages/sign_in.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Text('welcome');
-  }
+  runApp(const ProviderScope(
+    child:  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignIn(),
+    ),
+  ));
 }
---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:friends_tracker_app/pages/config.dart';
+import 'package:friends_tracker_app/api/google_sign_in_api.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:friends_tracker_app/api/friends_tracker_api.dart';
+import 'package:friends_tracker_app/pages/home.dart';
+import 'package:friends_tracker_app/providers/current_user_provider.dart';
+
+class SignIn extends ConsumerStatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  ConsumerState<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends ConsumerState<SignIn> {
+
+  Future signIn() async {
+    final googleUser = await GoogleSignInApi.login();
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final accessToken = googleAuth?.accessToken;
+    if(googleUser == null || googleAuth == null || accessToken == null){
+      if(mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Sign in failed')));
+      }
+    }else {
+      FriendsTrackerApi.authenticate(accessToken).then((user){
+        user.accessToken = accessToken;
+        ref.read(currentUserNotifierProvider.notifier).setCurrentUser(user);
+          if(mounted){
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const Home()));
+          }
+        // KaquizApi.getFriends(accessToken)
+        //     .then((friends) {
+        //   ref.read(friendsNotifierProvider.notifier).setFriends(friends ?? []);
+        //   if(mounted){
+        //     Navigator.of(context).pushReplacement(MaterialPageRoute(
+        //         builder: (context) => const Home()));
+        //   }
+        // })
+        //     .onError((Object error, StackTrace stackTrace){
+        //   print('ERROR: $error');
+        //   if(mounted) {
+        //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sign in failed: $error')));
+        //   }
+        // });
+      }).onError((Object error, StackTrace stackTrace){
+        print('ERROR: $error');
+        if(mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sign in failed: $error')));
+        }
+      });
+    }
+  }
+
+
+  void configHandler() {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const Config()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.teal.shade600,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Friends Tracker'),
+              IconButton(onPressed: configHandler, icon: const Icon(Icons.settings)),
+            ],
+          )
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome to Friends Tracker App',
+                style: TextStyle(fontSize: 28),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                  onPressed: signIn,
+                  child: const Text(
+                      'Sign In with Google',
+                      style: TextStyle(color: Colors.black, fontSize: 16)
+                  )),
+            ],
+          )
+      ),
+    );
+  }
+}
